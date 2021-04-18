4bfc4f510663bdf8c2eda194e3fd96ce14186d11
gaurav.bansode <gaurav.bansode@servicenow.com>
2021-04-17 20:55:53 -0500

    Adding dedupe logic
    Adding ternary
    
    moew new changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 console.log("This is a test");
+const deDupe = (arr) => (arr.length === 1 ? Array.from(new Set(arr)) : []);
+console.log(deDupe([1, 1, 1, 2, 3, 1, 2, 2, 3]));
+// new Changes here
+console.log("konichiwa");
